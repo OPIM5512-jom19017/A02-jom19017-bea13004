b335c73a248ad213a4d2ba574cb8d53d610581bd
OPIM5512-bea13004 <benjamin.akerele@uconn.edu>
2026-01-29 20:21:23 -0500

    Fixed error
    removed unnecessary .pyfile

--- Regressor.py
+++
@@ -1,8 +0,0 @@
-from sklearn.neural_network import MLPRegressor
-mlp = MLPRegressor(random_state=42,
-                   hidden_layer_sizes=(10),
-                   max_iter=200,
-                   batch_size=1000,
-                   activation="relu",
-                   validation_fraction=0.2,
-                   early_stopping=True) 
--- model.py
+++ model.py
@@ -18,3 +18,12 @@ y,
 test_size=0.2,
 random_state=42
 )
+
+from sklearn.neural_network import MLPRegressor
+mlp = MLPRegressor(random_state=42,
+                   hidden_layer_sizes=(10),
+                   max_iter=200,
+                   batch_size=1000,
+                   activation="relu",
+                   validation_fraction=0.2,
+                   early_stopping=True) 
